3afe766980d81cf89d350df111e86dee81bfb81c
smallwillie <73746484+smallwillie@users.noreply.github.com>
2021-05-02 15:46:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,11 +34,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
-    if(get_message=="hello"):
-        reply_text = "Hello I am AiotBpt!"
-    elif(get_message=="hi"):
-        reply_text = "https://imgur.com"
+    if(get_message=="組別"):
+        reply_text = "第五組"
+    elif(get_message=="組員"):
+        reply_text = "黃啟洲 M10913017\n黃瀚緯 B10613005\n鐘竣耀 M10913021\n李永漢 M10913047"
+    elif(get_message=="課程名稱"):
+        reply_text = "人工智慧暨物聯網系統設計"
     else:
-        reply_text = "Wrong keywords!"
+        reply_text = get_message
     message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
